18bed3849d0333e5b386316559e846b454605e83
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-28 05:31:23 +0900

    get campaign

--- test-pkg/app/controllers/csvs_controller.rb
+++ test-pkg/app/controllers/csvs_controller.rb
@@ -10,18 +10,14 @@ class CsvsController < ApplicationController
 
 	log = Logger.new(STDOUT)
 
-	columns = Answer.
-	  includes([:enq_question, :campaign]).
-	  order('user_id, user_agent, enq_questions.num').
+	columns = Campaign.
+	  includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
+	  order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
 	  where('answers.updated_at between ? and ?', params[:from], params[:to]).
-	  find_by_campaign_id(params[:campaign_id])
-	file_name = Kconv.kconv("answer.csv", Kconv::SJIS)
-	headers = Campaign.
-	  includes(:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches, :answers]]]]).
-	  order('enq_questions.num, choices.`order`').
 	  find_by_mid(params[:campaign_id])
+	file_name = Kconv.kconv("answer.csv", Kconv::SJIS)
 	header = ["キャンペーンID", "アンケートID", "回答日", "ユーザID", "User-Agent"]
-	headers.enq.enq_faces[0].enq_pages.each do |ep|
+	columns.enq.enq_faces[0].enq_pages.each do |ep|
 	  ep.enq_questions.each do |eq|
       header << "#{eq.seq}.#{eq.question.title}"
 	  end
@@ -29,11 +25,11 @@ class CsvsController < ApplicationController
 
 	csv_data = CSV.generate("", {:encoding => 'sjis', :row_sep => "\r\n", :headers => header, :write_headers => true}) do |csv|
       column = []
-      column << columns[:campaign_id]
-      column << columns.campaign[:enq_id]
-      column << columns[:updated_at]
-      column << columns[:user_id]
-      column << columns[:user_agent]
+      column << columns[:mid]
+      column << columns[:enq_id]
+      column << columns.answers[0][:updated_at]
+      column << columns.answers[0][:user_id]
+      column << columns.answers[0][:user_agent]
       csv << column
     end
 
