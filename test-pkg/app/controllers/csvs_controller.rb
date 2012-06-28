c5677e848ff7e18aa287cc06208a83543112dd3f
yasuhiroishikawa <yas-ishikawa@nadai.jp>
2012-06-28 10:08:07 +0900

    20120628

--- test-pkg/app/controllers/csvs_controller.rb
+++ test-pkg/app/controllers/csvs_controller.rb
@@ -7,40 +7,61 @@ class CsvsController < ApplicationController
     require 'kconv'
     require 'csv'
 
-	columns = Campaign.
-	  includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
-	  order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
-	  where('answers.updated_at between ? and ?', params[:from], params[:to]).
-	  find_by_mid(params[:campaign_id])
-	file_name = Kconv.kconv("answer.csv", Kconv::SJIS)
-	header = ["キャンペーンID", "アンケートID", "回答日", "ユーザID", "User-Agent"]
-	columns.enq.enq_faces[0].enq_pages.each do |ep|
-	  ep.enq_questions.each do |eq|
+#    if :from != null and params[:to] != null
+      columns = Campaign.
+        includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
+        order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
+        where('answers.updated_at between ? and ?', params[:from], params[:to]).
+        find_by_mid(params[:campaign_id])
+#    elsif params[:from].nil? and params[:to].nil
+#      columns = Campaign.
+#        includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
+#        order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
+#        where('answers.updated_at >= ?', params[:from]).
+#        find_by_mid(params[:campaign_id])
+#    elsif params[:from].nil and params[:to].nil?
+#      columns = Campaign.
+#        includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
+#        order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
+#        where('answers.updated_at <= ?', params[:to]).
+#        find_by_mid(params[:campaign_id])
+#    else
+#      columns = Campaign.
+#        includes([{:enq => [:enq_faces => [:enq_pages => [:enq_questions => [{:question => :choices}, :branches]]]]}, :answers]).
+#        order('answers.user_id, answers.user_agent, enq_questions.num, choices.`order`').
+#        find_by_mid(params[:campaign_id])
+#    end
+    
+    file_name = Kconv.kconv("answer.csv", Kconv::SJIS)
+    
+    header = ["キャンペーンID", "アンケートID", "回答日", "ユーザID", "User-Agent"]
+    columns.enq.enq_faces[0].enq_pages.each do |ep|
+      ep.enq_questions.each do |eq|
       header << "#{eq.seq}.#{eq.question.title}"
-	  end
-	end
+      end
+    end
 
-	csv_data = CSV.generate("", {:encoding => 'sjis', :row_sep => "\r\n", :headers => header, :write_headers => true}) do |csv|
+    csv_data = CSV.generate("", {:encoding => 'sjis', :row_sep => "\r\n", :headers => header, :write_headers => true}) do |csv|
       column = []
-	  tmp_name = ""
-	  tmp_agent = ""
-	  columns.answers.each do |ans|
-	    if tmp_name != ans[:user_name] or tmp_agent != ans[:user_agent]
-		  if tmp_name != ""
-		    csv << column
-		  end
-		  tmp_name = ans[:user_name]
-		  tmp_agent = ans[:user_agent]
-		  column = []
+      tmp_name = ""
+      tmp_agent = ""
+      columns.answers.each do |ans|
+        if tmp_name != ans[:user_name] or tmp_agent != ans[:user_agent]
+          if tmp_name != ""
+            csv << column
+          end
+          tmp_name = ans[:user_name]
+          tmp_agent = ans[:user_agent]
+          column = []
           column << columns[:mid]
           column << columns[:enq_id]
           column << ans[:updated_at]
           column << ans[:user_id]
           column << ans[:user_agent]
-		end
-		
-		column << ans[:answer]
-	  end
+        end
+        
+        column << ans[:answer]
+      end
       csv << column 
     end
 
